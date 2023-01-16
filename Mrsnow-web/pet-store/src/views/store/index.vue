b83b5db608b961402104e959d77b2a8e1ae92006
mrsnow <1505687245@qq.com>
2023-01-16 19:09:23 +0800

    搜索栏

--- Mrsnow-web/pet-store/src/views/store/index.vue
+++ Mrsnow-web/pet-store/src/views/store/index.vue
@@ -1,15 +1,18 @@
 <template>
-    <div>内容</div>
+    <search/>
+    <div class="content">内容</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import MyFooter from '../../components/layout/MyFooter.vue';
 import MyHeader from '../../components/layout/MyHeader.vue';
+import search from '../../components/store/content/search.vue';
 export default defineComponent({
     components: {
         MyHeader,
         MyFooter,
+        search
     },
     setup () {
         
@@ -20,5 +23,8 @@ export default defineComponent({
 </script>
 
 <style scoped>
-
+    .content {
+        height: 80vh;
+        width: 100%;
+    }
 </style>
